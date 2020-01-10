56b2e68c1745556132f98ff3b916d80a6d314228
Steven Hanson <steven.hanson@wintechnologies.net>
2020-01-10 15:02:47 +0000

    Add order of execution during mounting

---
+++ lifecycle/src/Components/LifecycleA.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+class LifecycleA extends Component {
+    constructor(props) {
+        super();
+
+        this.state = {
+            name: 'Name'
+        }
+
+        console.log('LifecycleA constructor');
+    }
+
+    static getDerivedStateFromProps(props, state) {
+        console.log('LifecycleA getDerivedStateFromProps')
+        return null;
+    }
+
+    componentDidMount() {
+        console.log('LifecycleA componentDidMount')
+    }
+
+    render() {
+        console.log('LifecycleA render')
+        return (
+            <div>
+                LifecycleA
+            </div>
+        )
+    }
+}
+
+export default LifecycleA;
